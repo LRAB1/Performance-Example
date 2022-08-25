81198933f6b8bd3c0af6cefdeae18cde44a9c7f6
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-08-25 13:21:29 +0200

    pm2 init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { fork } = require('child_process');
 const cluster = require('cluster')
+const os = require('os');
 
 const express = require('express');
 
@@ -24,11 +25,13 @@ app.get('/timer', (req, res) => {
     res.send(`Ding ding ding! ${process.pid}`);
 });
 
-console.log('Running server.js...');
+//console.log('Running server.js...');
 if(cluster.isMaster) {
     console.log('Master has been started');
-    cluster.fork();
-    cluster.fork();
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork()
+    }
 }   else {
     console.log('Worker process started.');
     app.listen(3000);
