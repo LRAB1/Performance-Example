a72360603a0a705565adabf96651ad024fa2c3a3
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-09-05 11:13:08 +0200

    pm2 implement

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const { fork } = require('child_process');
-const cluster = require('cluster')
-const os = require('os');
 
 const express = require('express');
 
@@ -14,25 +12,14 @@ function delay(duration) {
 };
 
 app.get('/', (req, res) => {
-//    JSON.stringify({}) => "{}"
-//    JSON.parse("") => {}
-//    [5,1,2,3,4].sort()
     res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-    delay(9000);
-    res.send(`Ding ding ding! ${process.pid}`);
+    delay(4000);
+    res.send(`Beep Beep Beep! ${process.pid}`);
 });
 
-//console.log('Running server.js...');
-if(cluster.isMaster) {
-    console.log('Master has been started');
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork()
-    }
-}   else {
-    console.log('Worker process started.');
-    app.listen(3000);
-    }
+console.log('Running server.js...');
+console.log('Worker process started.');
+app.listen(3000);
